d23ce674acd97daa14fd08eeee38fe965fb7c492
yizhiwazi <maven_work@163.com>
2017-10-02 23:49:06 +0800

    删除旧工程

--- springboot-data-jpa/src/main/java/com/hehe/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hehe.repository;
-
-import com.hehe.common.CommonRepository;
-import com.hehe.entity.User;
-
-
-public interface UserRepository extends CommonRepository<User,String> {
-}
