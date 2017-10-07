5f7b7b2ac45d4ae79cdd517e5c16494dd941d27e
yizhiwazi <maven_work@163.com>
2017-10-07 21:48:45 +0800

    SpringBoot使用JPA让数据库访问更简单优雅

--- springboot-data-jpa/src/main/resources/t_user.sql
+++ springboot-data-jpa/src/main/resources/t_user.sql
@@ -1,4 +1,6 @@
-
+DROP DATABASE IF EXISTS `socks`;
+CREATE DATABASE `socks`;
+USE `SOCKS`;
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `USER_ID` varchar(50) ,
@@ -7,4 +9,4 @@ CREATE TABLE `t_user` (
 ) ;
 
 INSERT INTO `t_user` VALUES ('1', 'admin', 'admin');
-INSERT INTO `t_user` VALUES ('2', 'hikari', 'buzhidao');
+INSERT INTO `t_user` VALUES ('2', 'yizhiwazi', '123456');
