402a86c1ff4ade561199700dc06b63d8e5418196
yizhiwazi <maven_work@163.com>
2017-10-11 23:29:40 +0800

    SpringBoot 全局异常处理(让控制层“减肥”)

--- springboot-global-exception/src/main/java/com/hehe/exception/GlobalExceptionHandler.java
+++ springboot-global-exception/src/main/java/com/hehe/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.hehe.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hehe.entity.R;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * 主要用途：用于捕捉全局异常(针对控制层),并返回指定的异常信息页(View)或异常信息(Json)。
@@ -22,7 +26,6 @@ import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
     private final static String EX_FALLBACK_VIEW = "exception";//指定异常信息页
 
     /**
@@ -30,14 +33,9 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(RuntimeException.class)
     public String runtimeExHandler(HttpServletRequest req, Exception ex, Model model) {
-        //获取HTTP状态码
-        Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
-        HttpStatus httpStatus= statusCode!=null?HttpStatus.valueOf(statusCode):HttpStatus.INTERNAL_SERVER_ERROR;
-        String exInfo=String.format("(type=%s,status=%s.)",httpStatus.getReasonPhrase(),httpStatus.value());
-        //返回相关信息
-        model.addAttribute("exInfo", exInfo);//HTTP状态码
         model.addAttribute("exUrl", req.getRequestURL().toString());//问题地址
         model.addAttribute("exMsg", ex.toString());//问题信息
+        model.addAttribute("exStatusInfo", getHttpStatusJson(req) );//Http状态
         return EX_FALLBACK_VIEW;
     }
 
@@ -46,8 +44,33 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public R defaultExHandler(HttpServletRequest req, Exception e) {
-        return R.isFail(e).data("全局异常JSON：服务异常,请稍后再试！！:" + req.getRequestURL());
+    public R defaultExHandler(HttpServletRequest req, Exception ex) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("exUrl", req.getRequestURL().toString());
+        map.put("exMsg", ex.toString());
+        map.put("exStatusInfo",getHttpStatusJson(req) );
+        return R.isOk().data(map);
+    }
+
+    public String getHttpStatusJson(HttpServletRequest req) {
+        HttpStatus httpStatus =getHttpStatus(req);
+        Map<String,Object> map = new LinkedHashMap<>();
+        map.put("status",httpStatus.value());
+        map.put("type",httpStatus.getReasonPhrase());
+        try {
+            return new ObjectMapper().writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+    public HttpStatus getHttpStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        try {
+            return statusCode!=null?HttpStatus.valueOf(statusCode):HttpStatus.INTERNAL_SERVER_ERROR;
+        } catch (Exception ex) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
     }
 
 }
