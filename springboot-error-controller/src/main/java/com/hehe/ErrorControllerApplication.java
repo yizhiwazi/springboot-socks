8600d63a9c790bfe1590394da4eb994aa48f1ac6
yizhiwazi <maven_work@163.com>
2017-10-18 01:03:57 +0800

    SpringBoot 统一异常处理(ErrorController+ErrorBuilder)

--- springboot-error-controller/src/main/java/com/hehe/ErrorControllerApplication.java
+++ springboot-error-controller/src/main/java/com/hehe/ErrorControllerApplication.java
@@ -3,7 +3,9 @@ package com.hehe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,18 +13,35 @@ import java.util.List;
  * @author yizhiwazi
  */
 @SpringBootApplication
+@RestController
 public class ErrorControllerApplication {
     /**
-     * 模拟用户数据访问
+     * 随机抛出异常.
+     */
+    private void randomException() throws Exception {
+        Exception[] exceptions = { //异常集合
+                new NullPointerException(),
+                new ArrayIndexOutOfBoundsException(),
+                new NumberFormatException(),
+                new SQLException()};
+        //发生概率
+        double probability = 0.75;
+        if (Math.random() < probability) {
+            //情况1：要么抛出异常
+            throw exceptions[(int) (Math.random() * exceptions.length)];
+        } else {
+            //情况2：要么继续运行
+        }
+
+    }
+
+    /**
+     * 模拟用户数据访问.
      */
     @GetMapping("/")
     public List index() throws Exception {
-        //概率指数
-        double probability = 0.6;
-        if ((Math.random()) < probability) {
-            throw new NullPointerException("空指针");
-        }
-        return Arrays.asList("用户数据1！", "用户数据2！你在刷新试试！");
+        randomException();
+        return Arrays.asList("正常用户数据1!", "正常用户数据2! 请按F5刷新!!");
     }
 
     public static void main(String[] args) {
--- springboot-error-controller/src/main/java/com/hehe/error/ErrorInfo.java
+++ springboot-error-controller/src/main/java/com/hehe/error/ErrorInfo.java
@@ -1,16 +1,12 @@
 package com.hehe.error;
-
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
-
 /**
  * 主要用途：存储错误信息的Bean.
  * 设计说明：
- * 1.官方提供了默认的错误信息和存储实现,但其最终返回的是存储结构是Map<String,Object>。
- * 2.为了避免采用Map这种模糊的数据结构,这里定制专门的Bean来存储错误信息。
+ * 1.官方提供了默认错误存储{@link DefaultErrorAttributes},但其最终返回的是存储结构是Map<String,Object>。
+ * 2.为了避免采用Map这种模糊的数据结构,故统一使用{@link ErrorInfo}来表示错误信息。
+ * 3.作为存储Bean,最好不要耦合业务逻辑,故统一使用{@link ErrorInfoBuilder}来构建错误信息.
  *
- * @see ErrorAttributes
- * @see DefaultErrorAttributes
+ * @author yizhiwazi
  */
 public class ErrorInfo {
     /**
