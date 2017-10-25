931cd015e9232afd14cc625152aa6b963ac570bb
yizhiwazi <maven_work@163.com>
2017-10-26 00:11:12 +0800

    SpringBoot 快速实现跨域访问(CORS)

---
+++ springboot-cross-orgin/yy-web/src/main/java/com/hehe/yyweb/config/GlobalCorsConfig.java
@@ -0,0 +1,20 @@
+package com.hehe.yyweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/s/**");
+            }
+        };
+    }
+}
---
+++ springboot-cross-orgin/yy-web/src/test/java/com/hehe/yyweb/YyWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hehe.yyweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YyWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
