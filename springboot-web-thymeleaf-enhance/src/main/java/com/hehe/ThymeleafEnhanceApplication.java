dfdb8fa27dc168484685c2b051ed6a2d62a7831e
socks <chenxiao1>
2018-07-16 22:10:43 +0800

    SpringBoot 使用WebJars统一管理静态资源

---
+++ springboot-web-thymeleaf-enhance/src/main/java/com/hehe/ThymeleafEnhanceApplication.java
@@ -0,0 +1,12 @@
+package com.hehe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafEnhanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafEnhanceApplication.class, args);
+    }
+}
