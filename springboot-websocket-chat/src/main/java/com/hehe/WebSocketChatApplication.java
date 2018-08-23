5b2338fe260bb1467dc51419399d3134f83156c2
socks <chenxiao1>
2018-08-24 01:33:27 +0800

    SpringBoot 使用WebSocket打造在线聊天室（基于注解）

--- springboot-websocket-chat/src/main/java/com/hehe/WebSocketChatApplication.java
+++ springboot-websocket-chat/src/main/java/com/hehe/WebSocketChatApplication.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class WebSocketChatApplication {
@@ -23,11 +27,14 @@ public class WebSocketChatApplication {
      * 聊天界面
      */
     @GetMapping("/index")
-    public ModelAndView index(String username, String password) {
+    public ModelAndView index(String username, String password, HttpServletRequest request) throws UnknownHostException {
         if (StringUtils.isEmpty(username)) {
             username = "匿名用户";
         }
-        return new ModelAndView("/chat", "username", username);
+        ModelAndView mav = new ModelAndView("/chat");
+        mav.addObject("username", username);
+        mav.addObject("webSocketUrl", "ws://"+InetAddress.getLocalHost().getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/chat");
+        return mav;
     }
 
     public static void main(String[] args) {
