763dce7c4a4960dc6389eb96087a39ed3a278896
maven_work@163.com <chenxiao123>
2019-05-05 20:36:28 +0800

    SpringBoot 使用WebSocket打造在线聊天室（基于注解）

--- springboot-websocket-chat/src/main/java/com/hehe/chat/Message.java
+++ springboot-websocket-chat/src/main/java/com/hehe/chat/Message.java
@@ -1,14 +1,10 @@
 package com.hehe.chat;
 
 import com.alibaba.fastjson.JSON;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 /**
  * WebSocket 聊天消息类
  */
-@AllArgsConstructor
-@Data
 public class Message {
 
     public static final String ENTER = "ENTER";
@@ -27,4 +23,54 @@ public class Message {
         return JSON.toJSONString(new Message(type, username, msg, onlineTotal));
     }
 
+    public Message(String type, String username, String msg, int onlineCount) {
+        this.type = type;
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+    }
+
+    public static String getENTER() {
+        return ENTER;
+    }
+
+    public static String getSPEAK() {
+        return SPEAK;
+    }
+
+    public static String getQUIT() {
+        return QUIT;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
 }
